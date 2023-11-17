3647c72efc40bb186bb784f1c80c0d1d942523a4
rikords <iamhakanduyar@gmail.com>
2023-11-17 12:05:34 +0300

    The commit changes the CSS of the Todo component. It increases the width and height of the Todo component, changes the margin-top, and removes the margin-bottom. It also adds a todo-header, todo-add, todo-input, todo-add-btn, and todoitems-cross-icons class.

--- src/Components/TodoItems.jsx
+++ src/Components/TodoItems.jsx
@@ -1,6 +1,35 @@
 import "./CSS/TodoItems.css";
-const TodoItems = () => {
-  return <div>TodoItems</div>;
+import tick from "./Assets/tick.png";
+import not_tick from "./Assets/not_tick.png";
+import cross from "./Assets/cross.png";
+
+const TodoItems = ({ no, display, text }) => {
+  const toggle = (no) => {
+    let data = JSON.parse(localStorage.getItem("todos "));
+    for (let i = 0; i < data.length; i++) {
+      if (condition) {
+      }
+    }
+  };
+
+  return (
+    <div className="todoitems">
+      <div
+        className="todoitems-container"
+        onClick={() => {
+          toggle(no);
+        }}
+      >
+        {display === "" ? (
+          <img src={not_tick} alt="" className="custom-img" />
+        ) : (
+          <img src={tick} alt="" className="custom-img" />
+        )}
+        <div className="todoitems-text">{text}</div>
+      </div>
+      <img className="todoitems-cross-icons" src={cross} alt="" />
+    </div>
+  );
 };
 
 export default TodoItems;
