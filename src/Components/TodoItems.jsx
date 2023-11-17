ce84db50bc97d79d5f43849b6504ca0fd4ef5ff1
rikords <iamhakanduyar@gmail.com>
2023-11-17 22:45:51 +0300

    The diff adds a cursor pointer to the todoitems-cross-icons class and a line-through to the line-through class.

--- src/Components/TodoItems.jsx
+++ src/Components/TodoItems.jsx
@@ -4,6 +4,12 @@ import not_tick from "./Assets/not_tick.png";
 import cross from "./Assets/cross.png";
 
 const TodoItems = ({ no, display, text, setTodos }) => {
+  const deleteTodo = (no) => {
+    let data = JSON.parse(localStorage.getItem("todos"));
+    data = data.filter((todo) => todo.no !== no);
+    setTodos(data);
+  };
+
   const toggle = (no) => {
     let data = JSON.parse(localStorage.getItem("todos"));
     for (let i = 0; i < data.length; i++) {
@@ -22,7 +28,7 @@ const TodoItems = ({ no, display, text, setTodos }) => {
   return (
     <div className="todoitems">
       <div
-        className="todoitems-container"
+        className={`todoitems-container ${display}`}
         onClick={() => {
           toggle(no);
         }}
@@ -34,7 +40,14 @@ const TodoItems = ({ no, display, text, setTodos }) => {
         )}
         <div className="todoitems-text">{text}</div>
       </div>
-      <img className="todoitems-cross-icons" src={cross} alt="" />
+      <img
+        className="todoitems-cross-icons"
+        onClick={() => {
+          deleteTodo(no);
+        }}
+        src={cross}
+        alt=""
+      />
     </div>
   );
 };
